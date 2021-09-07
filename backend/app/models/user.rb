6994102734c562971bb3e7fe88ea275dd43e99d9
ryota-sb <ryota.sb.1206@gmail.com>
2021-09-07 17:25:39 +0900

    リレーションの修正

--- backend/app/models/user.rb
+++ backend/app/models/user.rb
@@ -1,11 +1,11 @@
 class User < ApplicationRecord
   has_one :profile, dependent: :destroy
   
-  has_many :swipes, foreign_key: :to_user
-  has_many :to_users, through: :swipes, source: :from_user
-  has_many :reverse_of_swipes, class_name: 'Swipe', foreign_key: :from_user
-  has_many :from_users, through: :reverse_of_swipes, source: :to_user
-
+  has_many :swipes_from, class_name: 'Swipe', foreign_key: :from_user
+  has_many :swipes_to, class_name: 'Swipe', foreign_key: :to_user
+  has_many :active_swipes, through: :swipes_from, source: :to_user # 自分からのいいね
+  has_many :passive_swipes, through: :swipes_to, source: :from_user # 相手からのいいね
+  
   def self.from_token_payload(payload)
     find_by(sub: payload['sub']) || create!(sub: payload['sub'])
   end
