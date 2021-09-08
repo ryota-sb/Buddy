2be4d790267fd48a6789b5a5577225d36e92a811
ryota-sb <ryota.sb.1206@gmail.com>
2021-09-08 17:09:31 +0900

    message controller createアクション追加

--- backend/app/controllers/Api/V1/messages_controller.rb
+++ backend/app/controllers/Api/V1/messages_controller.rb
@@ -1,2 +1,17 @@
 class Api::V1::MessagesController < ApplicationController
+  def create
+    message = Message.new(message_params)
+
+    if message.save
+      render json: { status: 'success', message: message }
+    else
+      render json: { status: 'error', message: "作成に失敗しました" }
+    end
+  end
+
+  private
+
+  def message_params
+    params.premit(:chat_room_id, :user_id, :content)
+  end
 end
