6a1f401377f7e4c737f4823729b0be90cd96f00a
ryota-sb <ryota.sb.1206@gmail.com>
2021-09-08 17:10:10 +0900

    chat_room controller index show アクション追加

--- backend/app/controllers/Api/V1/chat_rooms_controller.rb
+++ backend/app/controllers/Api/V1/chat_rooms_controller.rb
@@ -1,2 +1,28 @@
 class Api::V1::ChatRoomsController < ApplicationController
+  before_action :set_chat_room, only: :show
+  
+  def index
+    chat_rooms = []
+
+    current_user.chat_rooms.order("created_at DESC").each do |chat_room|
+      chat_room: chat_room,
+      other_user: chat_room.users.where.not(id: current_user.id)[0]
+      last_message: chat_room.messages[-1]
+    end
+
+    render json: { status: 'success', chat_rooms: chat_rooms }
+  end
+
+  def show
+    other_user = @chat_room.users.where.not(id: current_user.id)[0]
+    messages = @chat_room.messages.order("created_at ASC")
+
+    render json: { status: 'success', other_user: other_user, messages: messages }
+  end 
+
+  private
+
+  def set_chat_room
+    @chat_room = ChatRoom.find(params[:id])
+  end
 end
